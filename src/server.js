d5470abb8bc1d939e88b33dcdc8557cbe53865c7
Randi <randyrisdiansyah@gmail.com>
2025-03-20 00:53:50 +0700

    refactor(server): improve validation and add pre response

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 const Hapi = require('@hapi/hapi');
-const NotesService = require('./services/inMemory/notesService');
-const notes = require('./api/notes');
+const Vision = require('@hapi/vision');
+const Handlebars = require('handlebars');
+
+const notes = require('./routes.controller');
+
+const { NotesService } = require('./services');
+const { NotesValidator } = require('./validator');
+const { ClientError } = require('./exceptions');
+const { BaseResponse } = require('./utils');
 
 const init = async () => {
   const notesService = new NotesService();
@@ -15,11 +22,36 @@ const init = async () => {
     },
   });
 
+  await server.register(Vision);
+
+  server.views({
+    engines: {
+      hbs: Handlebars,
+    },
+    path: __dirname + '/views/screens',
+  });
+
   await server.register({
     plugin: notes,
     options: {
       service: notesService,
+      validator: NotesValidator,
     },
+  });
+
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+
+    if (response instanceof ClientError) {
+      const newResponse = BaseResponse.create({
+        status: 'fail',
+        message: response.message,
+      });
+
+      return h.response(newResponse).code(response.statusCode);
+    }
+
+    return h.continue;
   });
 
   await server.start();
